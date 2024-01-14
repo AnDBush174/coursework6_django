494234cdea022f53d458767efb704732dfd3a586
Anna Raketa <rocketvsaliens@gmail.com>
2024-01-15 00:39:16 +0300

    Add CRUD for clients

--- clients/urls.py
+++ clients/urls.py
@@ -1,5 +1,13 @@
-from client.apps import ClientConfig
+from django.urls import path
 
-app_name = ClientConfig.name
+from clients.apps import ClientsConfig
+from clients.views import *
 
-urlpatterns = []
+app_name = ClientsConfig.name
+
+urlpatterns = [
+    path('view_all/', ClientListView.as_view(), name='all_clients'),
+    path('create/', ClientCreateView.as_view(), name='add_client'),
+    path('update/<int:pk>/', ClientUpdateView.as_view(), name='edit_client'),
+    path('delete/<int:pk>/', ClientDeleteView.as_view(), name='delete_client'),
+]
