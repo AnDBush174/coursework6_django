694226c1f4c7f9dd3926713c538d795a4b980b70
Anna Raketa <rocketvsaliens@gmail.com>
2024-01-20 23:12:19 +0300

    Add blog CRUD and templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import ArticleCreateView, ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+from blog.views import ArticleListView, ArticleDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='blog'),
-    path('create/', ArticleCreateView.as_view(), name='create_article'),
+    path('view_all/', ArticleListView.as_view(), name='blog'),
     path('view/<slug:slug>/', ArticleDetailView.as_view(), name='view_article'),
-    path('edit/<slug:slug>/', ArticleUpdateView.as_view(), name='edit_article'),
-    path('delete/<slug:slug>/', ArticleDeleteView.as_view(), name='delete_article'),
 ]
